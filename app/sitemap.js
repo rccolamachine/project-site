47b99582a5876f49317ab958fc543c103882fa72
rccolamachine <chapleski17@gmail.com>
2026-02-13 19:03:46 -0500

    adding sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,30 @@
+export default function sitemap() {
+  const baseUrl = "https://rccolamachine.com";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/photobooth`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/guestbook`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/button`,
+      lastModified: new Date(),
+    },
+  ];
+}
